0e97ad229eff1464a67ec3c04dc298fbc29e1852
Jacy <jacy.clare@gmail.com>
2012-10-02 17:13:00 -0400

    Migrations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Travelpod::Application.routes.draw do
+  resources :cities
+
+  resources :trips
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
