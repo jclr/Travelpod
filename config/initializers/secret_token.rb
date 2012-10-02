acf89bb0e83d9614a32d4e2a8d89decc0d210ce3
Jacy <jacy.clare@gmail.com>
2012-10-02 17:02:29 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelpod::Application.config.secret_token = 'c80b04d308eb8cfd873a5813723a4e2847b472fd739e8e5c86fe26c8f820a714658fd62f5165aa69302ee3ca694a3a1821fe742656ce07ba7b2c3ab6431b7693'
