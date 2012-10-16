e9461ced93a18847bf5e22430b4f84731dc08d3e
Jacy <jacy.clare@gmail.com>
2012-10-16 19:54:30 -0400

    Database issues

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,12 @@
 class Trip < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :city, :date, :lat, :lon, :post, :user
+  attr_accessible :city, :date, :lat, :lon, :post, :user_id, :user
+
+    def user_name
+        user.name if user
+    end
+
+    def user_id
+        user.id if user
+    end
 end
--- db/migrate/20121002211101_create_trips.rb
+++ db/migrate/20121002211101_create_trips.rb
@@ -1,6 +1,7 @@
 class CreateTrips < ActiveRecord::Migration
   def change
     create_table :trips do |t|
+      t.integer :user_id
       t.text :post
       t.decimal :lat
       t.decimal :lon
