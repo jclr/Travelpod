cf4e4129f928473dd663e8372d120f4c03c91ca6
Jacy <jacy.clare@gmail.com>
2012-11-02 16:51:03 -0400

    Added city seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+City.create(:name => "Philadelphia", :lat => 1, :lon => 1)
